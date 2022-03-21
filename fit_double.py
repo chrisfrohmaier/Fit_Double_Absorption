dc292e0e0b470a1cbdcd6d2000daef7752592d99
chrisfrohmaier <c.frohmaier@soton.ac.uk>
2022-03-21 16:05:19 +0000

    First commit

---
+++ fit_double.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import lmfit
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy import constants as const
+import astropy.units as u
+from mpl_point_clicker import clicker
+from matplotlib.widgets import Button
+
